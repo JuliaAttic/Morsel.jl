349a1399a42ee533cd5fe090830b12ac465a2cd2
Nick Collins <nick@vividx.co.uk>
2013-03-27 12:40:29 -0400

    Corrections and new function descriptions
    Descriptions for `App`, `prepare_response`, `start` and the `route`
    shortcut functions.

--- src/Micro.jl
+++ src/Micro.jl
@@ -29,10 +29,16 @@ export App,
 
 include("Routes.jl")
 
-# The produces a dictionary that maps each type of request (GET, POST, etc.) to
-# a RoutingTable, which is an alias to the Tree datatype specified in Trees.jl.
-routing_tables() = (HttpMethodBitmask => RoutingTable)[method => RoutingTable() for method in HttpMethodBitmasks]
+# This produces a dictionary that maps each type of request (GET, POST, etc.)
+# to a `RoutingTable`, which is an alias to the `Tree` datatype specified in
+# `Trees.jl`.
+routing_tables() = (HttpMethodBitmask => RoutingTable)[method => RoutingTable()
+                                            for method in HttpMethodBitmasks]
 
+# An 'App' is simply a dictionary linking each HTTP method to a `RoutingTable`.
+# The detault constructor produces an empty `RoutingTable` for member of
+# `HttpMethods`.
+#
 type App
     routes::Dict{HttpMethodBitmask, RoutingTable}
 end
@@ -40,20 +46,23 @@ function app()
     App(routing_tables())
 end
 
-# This defines a route and adds it to the routes dictionary. As HTTP methods
-# are bitmasked integers they can be combined using the bitwise or opperator,
-# e.g. "GET | POST" will add a GET method and a POST method.
+# This defines a route and adds it to the `app.routes` dictionary. As HTTP
+# methods are bitmasked integers they can be combined using the bitwise or
+# opperator, e.g. `GET | POST` refers to a `GET` method and a `POST` method.
 #
 # Example:
+#
 #   function hello_world(req, res)
 #       "Hello, world!"
 #   end
 #   route(hello_world, GET | POST, "/hello/world")
 #
 # Or using do syntax:
+#
 #   route(app, GET | POST, "/hello/world") do req, res
 #       "Hello, world"
 #   end
+#
 function route(handler::Function, app::App, methods::Int, path::String)
     for method in HttpMethodBitmasks
         methods & method == method && register!(app.routes[method], path, handler)
@@ -62,6 +71,9 @@ function route(handler::Function, app::App, methods::Int, path::String)
 end
 route(a::App, m::Int, p::String, h::Function) = route(h, a, m, p)
 
+# These are shortcut functions for common calls to `route`.
+# e.g `get` calls `route` with a `GET` as the method parameter.
+#
 get(h::Function, a::App, p::String)    = route(h, a, GET, p)
 post(h::Function, a::App, p::String)   = route(h, a, POST, p)
 put(h::Function, a::App, p::String)    = route(h, a, PUT, p)
@@ -72,11 +84,20 @@ url_params(req::Request)                       = req.state[:url_params]
 url_params(req::Request, key::String, default) = get(req.state[:url_params], key, default)
 url_params(req::Request, key::String)          = url_params(req, key, nothing)
 
+# `prepare_response` simply sets the data field of the `Response` to the input
+# string `s` and calls the middleware's `repsond` function.
+#
 function prepare_response(s::String, req::Request, res::Response)
     res.data = s
     respond(req, res)
 end
 
+# `start` uses to `Http.jl` and `Meddle.jl` libraries to launch a webserver
+# running `app` on the desired `port`.
+#
+# This is a blocking function, anything that appears after it in the source
+# file will not run.
+#
 function start(app::App, port::Int)
 
     MicroApp = Midware() do req::Request, res::Response
