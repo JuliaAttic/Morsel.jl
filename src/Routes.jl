f3d87d9a16a9f55cbc092ebb8d7d51209f1bce8b
Daniel Mendel <daniel@danielespeset.com>
2013-04-01 01:12:34 -0400

    Documents DyanmicRoutes & clarifies their syntax

--- src/Routes.jl
+++ src/Routes.jl
@@ -3,17 +3,42 @@ include("Trees.jl")
 typealias Params Dict{Any,Any}
 
 abstract RouteNode
-getparams(params::Params, v::RouteNode, p::String) = params
 
+# StringNodes store all static route parts
+# For example, route "/foo/bar/" is comprised of 2 StringNodes, "foo" and "bar"
+#
 immutable StringNode <: RouteNode
     val::String
 end
+# Equality & Matching for building / traversing the routing table
 isequal(s1::StringNode, s2::String)     = s1.val == s2
 isequal(s1::StringNode, s2::StringNode) = s1.val == s2.val
 ismatch(s1::StringNode, s2::String)     = isequal(s1, s2)
 
+# Returns the name part of a RegEx or DataType dynamic route
+# Will return `foo` in `<foo::String>`
+#
 getname(p::String) = Base.ismatch(r"^<([^\:]*)::", p) ? match(r"^<([^\:]*)::", p).captures[1] : nothing
 
+# DynamicNodes
+#
+# Route search uses `regex` to validate matches,
+# then adds key `name` to `req.state[:params]` with unique value from
+# resource after it is ( optionally ) processed through `convert`.
+#
+# DynamicNodes are used for three kinds of nodes:
+#
+#   - NamedParamNodes:     "/foo/<bar>" 
+#            => matches    "/foo/*", adds `req.params[:bar]::String`
+#
+#   - RegexNodes:          "/foo/<bar::%[0-9][0-9]-[A-Z]{5}>"
+#            => matches    "/foo/00-ABCDE", adds `req.params[:bar]::String`
+#            => wont match "/foo/00-ABCDEF", "foo/0-ABCDE"
+#
+#   - DataTypeNodes:       "/foo/<bar::Int>"
+#            => matches    "/foo/10", adds `req.params[:bar]::Int`
+#            => wont match "/foo/10.0", "foo/abc"
+#
 immutable DynamicNode <: RouteNode
     name::String
     regex::Regex
@@ -22,18 +47,29 @@ end
 DynamicNode(n::String, r::Regex)    = DynamicNode(n,r,nothing)
 DynamicNode(p::String, c::Function) = DynamicNode(getname(p), Regex("^$(match(r":%([^>]*)>", p).captures[1])\$"), c)
 
-RegexNode(p::String) = DynamicNode(getname(p), Regex("^$(match(r":%([^>]*)>", p).captures[1])\$"))
-NamedParamNode(p::String) = DynamicNode(p[2:length(p)-1], r".*")
-const DataTypeNodeBuilders = (String => (Regex, Function))[ 
-    "Int"   => (r"^[0-9]*$", int),
-    "Float" => (r"^[0-9]*\.[0-9]*", float)
-]
-DataTypeNode(p::String, t::String) = DynamicNode(getname(p), DataTypeNodeBuilders[t]...)
-
+# Equality & Matching for building / traversing the routing table
 isequal(s1::DynamicNode, s2::String)      = Base.ismatch( s1.regex, s2 )
 isequal(s1::DynamicNode, s2::DynamicNode) = s1.regex.pattern == s2.regex.pattern && s1.name == s2.name
 ismatch(s1::DynamicNode, s2::String)      = isequal( s1, s2 )
-getparams(params::Params, v::DynamicNode, p::String) = params[symbol(v.name)] = v.convert == nothing ? p : v.convert(p)
+
+# NamedParam & Regex node constructors, these are NOT types.
+NamedParamNode(p::String) = DynamicNode(p[2:length(p)-1], r".*")
+RegexNode(p::String)      = DynamicNode(getname(p), Regex("^$(match(r":%([^>]*)>", p).captures[1])\$"))
+
+# All valid DataTypeNode types -- provides `regex` validator + converter.
+# Should be exposed to users for extension with custom DataTypes?
+#
+const DataTypeNodeBuilders = (String => (Regex, Function))[ 
+    "Int"   => (r"^[0-9]*$", int),
+    "Float" => (r"^[0-9]*\.[0-9]*$", float),
+    "String"=> (r"^.*$", string)
+]
+# DataType node constructor, also NOT a type.
+DataTypeNode(p::String, t::String) = DynamicNode(getname(p), DataTypeNodeBuilders[t]...)
+
+# Build the params dataset – dispatch needed for both route types.
+extend_params(params::Params, v::RouteNode, p::String) = params
+extend_params(params::Params, v::DynamicNode, p::String) = params[symbol(v.name)] = v.convert == nothing ? p : v.convert(p)
 
 typealias Route (RouteNode,Union(Function,Nothing)) # ('/about', function()...)
 isequal(r::Route, v) = isequal(r[1], v)
@@ -45,12 +81,16 @@ RoutingTable() = RoutingTable((StringNode("/"), nothing))
 
 ismatch(node::String, resource_chunk::String) = node == resource_chunk
 
+# Regex matchers for determining DynamicRoute types =>
+# Functions for building matching type from `part`.
+#
 const dynamic_route_dispatch = (Regex => Function)[
     r"^<[^:%]*::%"          => RegexNode,
-    r"^<[^:>]*::([^%>]*)>$" => part -> DataTypeNode(part, match(r"^<[^:>]*::([^%>]*)>$", part).captures[1]),
+    r"^<[^:>]*::[^%>]*>$"   => part -> DataTypeNode(part, match(r"^<[^:>]*::([^%>]*)>$", part).captures[1]),
     r"^<[^:>]*>$"           => NamedParamNode
 ]
 
+# Run for each part of a route, builds RouteNodes.
 function parse_part(part::String)
     if length(part) > 0 && Base.ismatch(r"^<[^>]*>$", part) 
         for v in dynamic_route_dispatch
@@ -120,7 +160,7 @@ end
 function searchroute(parts::Array, params::Params)
     function searchpred(val)
         if ismatch(val, parts[1])
-            params = getparams(params, val[1], parts[1])
+            params = extend_params(params, val[1], parts[1])
             if length(parts) == 1
                 true
             else
